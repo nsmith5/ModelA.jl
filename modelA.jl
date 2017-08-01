e6f830e948bb44e7b7e1ad283f71a5a9bbdc9b3c
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-08-01 11:34:46 -0700

    Reformat a bit

--- modelA.jl
+++ modelA.jl
@@ -3,78 +3,72 @@ module modelA
 export model, parameters, step!
 
 const kb = 1.0                # Boltzmann Constant
-kb::Float64                   # Type assert
+
 
 type model
-  # ------------------------------------
-  #  Set of thermodynamic parameters
-  # ------------------------------------
-  W0::Float64   # Laplacian coefficient
-  a2::Float64   # Second order field coefficient
-  a4::Float64   # Fourth order field coefficient
-  M ::Float64   # Mobility
-  T::Float64    # Temperature
+    # ------------------------------------
+    #  Set of thermodynamic parameters
+    # ------------------------------------
+    W0::Float64   # Laplacian coefficient
+    a2::Float64   # Second order field coefficient
+    a4::Float64   # Fourth order field coefficient
+    M ::Float64   # Mobility
+    T::Float64    # Temperature
 end
+
 
 type parameters
-  # ---------------------------
-  #   Physical parameters
-  # ---------------------------
-  dx::Float64   # Size of the space step
-  dt::Float64   # Size of the time step
+  	# ---------------------------
+  	#   Physical parameters
+  	# ---------------------------
+  	dx::Float64   # Size of the space step
+  	dt::Float64   # Size of the time step
 end
 
-function ring(i::Int64, dim::Int64)
-	#--------------------------------
-	# Modular arithmetic for the Ising
-	# model update. Helps define
-	# nearest neighbours for periodic
-	# boundary conditions
-	#--------------------------------
-  if i<=dim && i>=1
-    return i
-  elseif i==0
-    return dim
-  elseif i==dim+1
-    return 1
-  else
-    return -99
-  end
+
+function ∇²(ϕ, prmtr::parameters)
+	N = size(ϕ)[1]
+  	dx = prmtr.dx
+  	temp = zeros(N,N)
+	  
+	for i in 2:(N-1)
+    	for j in 2:(N-1)
+      		temp[i,j] = (ϕ[i+1,j] - 2 * ϕ[i,j] + ϕ[i - 1, j])/dx^2 + 
+                (ϕ[i,j+1]-2ϕ[i,j]+ϕ[i,j-1]) /dx^2
+    	end
+  	end
+	  
+	for i in 1:N
+		temp[i,N] = (ϕ[mod1(i+1,N), N] - 2 * ϕ[mod1(i,N), N] + ϕ[mod1(i-1,N), N] ) / dx ^ 2 +
+			(ϕ[mod1(i,N), 1] - 2 * ϕ[mod1(i,N), N] + ϕ[mod1(i,N), N-1]) / dx ^ 2
+		temp[i,1] = (ϕ[mod1(i+1, N), 1] - 2 * ϕ[mod1(i, N), 1] + ϕ[mod1(i-1, N), 1]) / dx ^ 2 +
+			(ϕ[mod1(i, N),2] - 2 * ϕ[mod1(i, N), 1] + ϕ[mod1(i, N), N]) / dx ^ 2
+  	end
+	  
+	for j in 1:N
+    	temp[N,j] =(ϕ[ N, mod1(j+1,N)] -2*ϕ[ N, mod1(j,N)]+ϕ[N, mod1(j-1,N)])/dx^2+
+                   (ϕ[1, mod1(j,N)] -2*ϕ[ N, mod1(j,N)]+ϕ[N-1, mod1(j,N)])/dx^2
+    	temp[1,j] =(ϕ[1,mod1(j+1,N)]-2*ϕ[1,mod1(j,N)]+ϕ[1, mod1(j-1,N)])/dx^2 +
+                   (ϕ[2, mod1(j,N)]-2*ϕ[1,mod1(j,N)]+ϕ[N, mod1(j,N)]) /dx^2
+  	end
+
+  	return temp
 end
 
-function ∇²(ϕ::Array{Float64, 2}, prmtr::parameters)
-  N = size(ϕ)[1]
-  dx = prmtr.dx
-  temp = zeros(N,N)
-  for i in 2:(N-1)
-    for j in 2:(N-1)
-      temp[i,j] = (ϕ[i+1,j]-2*ϕ[i,j]+ϕ[i-1,j])/dx^2 + (ϕ[i,j+1]-2ϕ[i,j]+ϕ[i,j-1]) /dx^2
-    end
-  end
-  for i in 1:N
-    temp[i,N] = (ϕ[ ring(i+1,N), N]-2*ϕ[ ring(i,N), N]+ϕ[ ring(i-1,N), N])/dx^2+(ϕ[ ring(i,N),1]-2ϕ[ ring(i,N), N]+ϕ[ ring(i,N), N-1]) /dx^2
-    temp[i,1] = (ϕ[ ring(i+1,N), 1]-2*ϕ[ ring(i,N), 1]+ϕ[ ring(i-1,N), 1])/dx^2+(ϕ[ ring(i,N),2]-2ϕ[ring(i,N),1]+ϕ[ring(i,N),N]) /dx^2
-  end
-  for j in 1:N
-    temp[N,j] =(ϕ[ N, ring(j+1,N)] -2*ϕ[ N, ring(j,N)]+ϕ[N, ring(j-1,N)])/dx^2+(ϕ[ 1, ring(j,N)] -2*ϕ[ N, ring(j,N)]+ϕ[N-1, ring(j,N)])/dx^2
-    temp[1,j] =(ϕ[1,ring(j+1,N)]-2*ϕ[1,ring(j,N)]+ϕ[1, ring(j-1,N)])/dx^2 +(ϕ[2, ring(j,N)]-2*ϕ[1,ring(j,N)]+ϕ[N, ring(j,N)]) /dx^2
-  end
 
-  return temp
-end
+function step!(ϕ, mod::model, prmtr::parameters)
+    # ----------------------
+    #   Euler time march
+    # the field by one time
+    # step.
+    # ----------------------
+    N = size(ϕ)[1]            # Size of the field
 
-function step!(ϕ::Array{Float64, 2}, mod::model, prmtr::parameters)
-  # ----------------------
-  #   Euler time march
-  # the field by one time
-  # step.
-  # ----------------------
-  N = size(ϕ)[1]            # Size of the field
-
-  ζ = sqrt(2*mod.M*kb*mod.T)*randn(N,N)   # Generate noise
-
-  ϕ += prmtr.dt * (mod.M * (mod.W0^2 * ∇²(ϕ, prmtr)-mod.a2*ϕ-mod.a4*ϕ.^3) + ζ)         # Update the field phi
-  return ϕ
+    ζ = sqrt(2*mod.M*kb*mod.T)*randn(N,N)   # Generate noise
+    ∇²ϕ = ∇²(ϕ, prmtr) 
+   
+    ϕ .+= prmtr.dt * (mod.M * (mod.W0 * ∇²ϕ .- mod.a2 * ϕ .- mod.a4 * ϕ.^3) .+ ζ)         # Update the field phi
+    return ϕ
 end
 
 end
--- script.jl
+++ script.jl
@@ -4,6 +4,7 @@ module Testing
 
 using modelA
 using PyPlot
+ion()
 
 W_0 = 0.50      # Derivative constant
 a2 = -1.0       # Second order constant
@@ -14,21 +15,7 @@ kbT = 0.4       # Temperature (for noise)
 dx = 0.8        # Lattice spacing
 dt = 0.1        # Time step size
 
-#  INITIAL conditions
-#  Uncomment to choose one:
-
-#  (1) Box
-# N = 500
-# ϕ = ones(500, 500)      # Initial Condition of the order parameter
-#
-# for i in N/4:3N/4
-#   for j in N/4:3N/4
-#     ϕ[i,j] = -1.0
-#   end
-# end
-
-# (2) Random (Default)
-
+# Initial Conditions
 N = 500
 ϕ = 0.5*randn(500, 500)
 
@@ -37,12 +24,13 @@ mod = model(W_0, a2, a4, M, kbT)
 par = parameters(dx, dt)
 
 c = imshow(ϕ)
-plt.colorbar()
+plt[:colorbar]()
+
 for x in 1:1000
-  ϕ = step!(ϕ, mod, par)
-  c[:set_data](ϕ)
-  draw()
-  println(x)
+    step!(ϕ, mod, par)
+    c[:set_data](ϕ)
+    draw()
+    println(x)
 end
 
 
